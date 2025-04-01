0ff0994ea7f48d6a822fc2c7892c4f05ac58ae19
drskiboss <skiboss@big-skis.com>
2025-04-02 00:26:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 This tells Vite to use relative paths
   plugins: [react()],
 })
